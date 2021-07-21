e549e57e7bdc3052d5035b8f64b1dccf0c982bc7
utkarsh <utkarshnanda9@gmail.com>
2021-07-22 00:05:15 +0530

    Added sticky navbar with smooth scrolling enabled

--- js/main.js
+++ js/main.js
@@ -29,4 +29,21 @@ $(document).ready(function () {
       },
     },
   });
+
+  // sticky navigation menu
+  let nav_offset_top = $(".header_area").height() + 100;
+
+  function navbarFixed() {
+    if ($(".header_area").length) {
+      $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if (scroll >= nav_offset_top) {
+          $(".header_area .main-menu").addClass("navbar_fixed");
+        } else {
+          $(".header_area .main-menu").removeClass("navbar_fixed");
+        }
+      });
+    }
+  }
+  navbarFixed();
 });
